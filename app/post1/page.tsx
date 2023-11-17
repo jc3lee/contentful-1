66928aed716b4b2778d847fb2cb0f360544c5498
dev <dev.if.ljc@gmail.com>
2023-11-17 22:43:29 +0100

    rich text render image

--- app/post1/page.tsx
+++ app/post1/page.tsx
@@ -1,8 +1,9 @@
 import { getClient } from "@/lib/contentful-client";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { Document } from "@contentful/rich-text-types";
+import { BLOCKS, Document } from "@contentful/rich-text-types";
 import Image from "next/image";
 import { ContentfulLivePreview } from "@contentful/live-preview";
+import { log } from "console";
 
 async function Post1() {
   const client = getClient();
@@ -12,7 +13,25 @@ async function Post1() {
   const { mainPhoto } = data.fields as any;
   const imgUrl = mainPhoto?.fields?.file?.url;
   const richTextComponents = documentToReactComponents(
-    data.fields.postContent as Document
+    data.fields.postContent as Document,
+    {
+      renderNode: {
+        [BLOCKS.EMBEDDED_ASSET]: (node) => {
+          log("node", JSON.stringify(node.data, null, 2));
+          const assetUrl = node.data.target?.fields?.file?.url;
+          const assetTitle = node.data.target?.fields?.title;
+          return (
+            <Image
+              src={assetUrl ? "https:" + assetUrl + "?w=1500&h=600" : ""}
+              alt={assetTitle || "Image not found"}
+              width={1500}
+              height={600}
+              className="mt-20 w-full aspect-[16/9] object-cover rounded-md"
+            />
+          );
+        },
+      },
+    }
   );
   return (
     <div className="max-w-screen-md mx-auto mt-8">
